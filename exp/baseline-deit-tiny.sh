2130af80fb4e5ff1a15288dafaaf179972789923
serizard <serizard1005@gmail.com>
2025-02-05 17:35:30 +0000

    feat: assign master port argument to experiment scripts

--- exp/baseline-deit-tiny.sh
+++ exp/baseline-deit-tiny.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [[ $# -eq 1 ]]; then
+if [[ $# -eq 2 ]]; then
     GPU_IDS=$1
+    MASTER_PORT=$2
 else
-    echo "Usage: $0 GPU_IDS (example: 0,1,2,3)"
+    echo "Usage: $0 GPU_IDS (example: 0,1,2,3) MASTER_PORT (example: 29501)"
     exit 1
 fi
 
 NUM_GPUS=$(echo $GPU_IDS | tr ',' '\n' | wc -l)
 
-
-CUDA_VISIBLE_DEVICES=$GPU_IDS torchrun --nproc_per_node=$NUM_GPUS tools/train.py \
+CUDA_VISIBLE_DEVICES=$GPU_IDS torchrun --nproc_per_node=$NUM_GPUS --master_port $MASTER_PORT tools/train.py \
     --student-model deit_tiny_patch16_224 \
     --teacher-model deit_small_distilled_patch16_224 \
     --dataset cifar-100 \
